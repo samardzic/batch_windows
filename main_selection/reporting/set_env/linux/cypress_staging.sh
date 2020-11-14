0ca01941d03fac8c8a9a2cac5a23970658a1ac90
uia66114 <Klikeri_181>
2020-11-15 00:05:28 +0100

    set envinronment

---
+++ main_selection/reporting/set_env/linux/cypress_staging.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------
+# TITLE        : Test Data for STAGING env
+# DESCRIPTION  : Modify test data to acomodate "staging" envinronment
+# PROJECT      : RVD
+# VERSION      : 0.1
+# ---------------------------------------------------------------------
+
+
+
+#  ---------------  Variables   ------------------------
+
+# Project ROOT path
+ROOT_PATH=~/Build/
+
+# CYPRESS test folder location
+CYPRESS_LOCATION=rvd_demo_portal
+
+# NODE_MODULES location
+NODE_LOCATION=~/node_modules/cypress/bin
+
+# Test data ACTIVE location
+TESTDATA_ACTIVE_LOCATION=cypress/test_data
+
+# Test data STAGING location
+TESTDATA_STAGING_LOCATION=cypress/test_data/staging
+
+
+
+
+# ---------------  Setting STAGING env as active   ------------------------
+
+# CD to Cypress test folder 
+cd $ROOT_PATH/$CYPRESS_LOCATION
+
+# Deletes existing ACTIVE test data file
+rm $ROOT_PATH/$CYPRESS_LOCATION/$TESTDATA_ACTIVE_LOCATION/testData.active.js
+
+# Copies STAGING test data file ACTIVE test data folder
+cp $ROOT_PATH/$CYPRESS_LOCATION/$TESTDATA_STAGING_LOCATION/test_data_staging.js $ROOT_PATH/$CYPRESS_LOCATION/$TESTDATA_ACTIVE_LOCATION
+
+# Renames STAGING test data file to ACTIVE test data file, and sets it as ACTIVE test data file
+mv $ROOT_PATH/$CYPRESS_LOCATION/$TESTDATA_ACTIVE_LOCATION/test_data_staging.js $ROOT_PATH/$CYPRESS_LOCATION/$TESTDATA_ACTIVE_LOCATION/testData.active.js
+
+
+
+# ---------------  Executing tests ------------------------
+
+$NODE_LOCATION/cypress run --headed
+# $NODE_LOCATION/cypress run --spec "c:\rvd\rvd_demo_portal\cypress\specs\*.*"
+
+
+
+
+
+
+
---
+++ main_selection/reporting/set_env/windows/cypress_staging.bat
@@ -0,0 +1,48 @@
+:: ==================================================================================
+:: TITLE        : Test Data for STAGING env
+:: DESCRIPTION  : Modify test data to acomodate "staging" envinronment
+:: PROJECT      : RVD
+:: VERSION      : 0.1
+:: ==================================================================================
+
+
+:: ---------------  Variables   ------------------------
+
+:: Project ROOT path
+set root_path=c:\rvd
+
+:: CYPRESS folder location
+set cypress_location=rvd_demo_portal
+
+:: NODE_MODULES location
+set node_location=c:\rvd\rvd_demo_portal\node_modules\.bin
+
+:: Test data ACTIVE location
+set test_data_active=cypress\test_data
+
+
+:: Test data STAGING location
+set test_data_staging=cypress\test_data\staging
+
+
+
+:: ---------------  Setting STAGING env as active   ------------------------
+
+:: CD to Cypress folder 
+cd %root_path%\%cypress_location%
+
+:: Deletes existing ACTIVE test data file
+del %root_path%\%cypress_location%\%test_data_active%\testData.active.js
+
+:: Copies STAGING test data file ACTIVE test data folder
+xcopy %root_path%\%cypress_location%\%test_data_staging%\test_data_staging.js %root_path%\%cypress_location%\%test_data_active%
+
+:: Renames STAGING test data file to ACTIVE test data file, and sets it as ACTIVE test data file
+rename %root_path%\%cypress_location%\%test_data_active%\test_data_staging.js testData.active.js
+
+
+
+:: ---------------  Executing tests ------------------------
+
+:: cmd /k %node_location%\cypress run --headed
+Cmd /k %node_location%\cypress run --spec "c:\rvd\rvd_demo_portal\cypress\specs\*.*"
